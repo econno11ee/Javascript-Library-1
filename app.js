476c22d945d079a7a7fb664dc6e2b98af9131080
Erin Connolly <erin.a.connolly@gmail.com>
2017-04-24 12:05:53 -0600

    setting up instances, hardcoding books

--- app.js
+++ app.js
@@ -4,7 +4,28 @@ function newBook(title, author, numberOfPages,publishDate){
   this.numberOfPages=numberOfPages;
   this.publishDates=publishDate;
 }
-  var Library = function() {
+  var library = function() {
 
   }
-  library.prototype.bookArray = new Array();
+
+
+
+  function lookAtStuff () {
+    console.log(book1);
+  }
+
+  library.prototype.myBookArray = new Array();
+  library.prototype.addBook = function (){};
+  library.prototype.removeBookByTitle = function (){};
+  library.prototype.removeBookByAuthor = function (){};
+  library.prototype.getRandomBook = function (){};
+  library.prototype.getBookByTitl = function (){};
+  library.prototype.getBooksByAuthor = function (){};
+  library.prototype.getAuthors = function (){};
+  library.prototype.getRandomAuthorName = function (){};
+
+  var comedyLibrary = new library();
+  var book1 = new newBook("1987","George Orwell", 3, 1986);
+  var book2 = new newBook("Duck Farm","George Orwell", 3, 1986);
+  var book3 = new newBook("The Crucible","Arther Miller", 3, 1986);
+  
