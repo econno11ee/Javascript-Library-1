7afb2259c68b8efe451ff8d91d921f436ca3a6d4
Erin Connolly <erin.a.connolly@gmail.com>
2017-05-05 12:56:36 -0600

    final changes

--- js/app.js
+++ js/app.js
@@ -19,64 +19,6 @@ library.prototype.myBookArray = new Array();
 
 
 
-library.prototype.removeBookByAuthor = function (authorName) {
-    var booksRemoved = 0;
-    //same as removeBookByTitle but need to run loop backwards to remove more than one
-    for (var i = this.myBookArray.length-1; i>=0; i--) {
-        if (authorName == this.myBookArray[i].author) {
-            this.myBookArray.splice(i,1);
-            booksRemoved++; //add to booksRemoved if an author matches but don't exit loop until all books are checked
-        } //keep looking for author matches until you get to the book at index 0 posiiton
-    }
-    if (booksRemoved > 0) { return true;}
-    return false;
-};
-
-
-
-
-
-library.prototype.getBookByTitle = function (title) {
-    var booksThatMatch = new Array();
-    for (var i = 0; i<this.myBookArray.length; i++) {
-        var titlesInLibrary = this.myBookArray[i].title;
-        //match is an easy way to check if a string matches any part of another string
-        if (titlesInLibrary.toLowerCase().match(title.toLowerCase()) !== null) {
-            booksThatMatch.push(this.myBookArray[i]);
-        }
-    }//2nd leg of if statement has to be outside of loop to get more than one match
-    return booksThatMatch;
-};
-
-library.prototype.getBooksByAuthor = function (author) {
-    var booksWithAuthorsThatMatch = new Array();
-    for (var i = 0; i<this.myBookArray.length; i++) {
-        var authorInLibrary = this.myBookArray[i].author;
-        if (authorInLibrary.toLowerCase().match(author.toLowerCase()) !== null) {
-            booksWithAuthorsThatMatch.push(this.myBookArray[i]);
-        }
-    }//2nd leg of if statement has to be outside of loop to get more than one match
-    return booksWithAuthorsThatMatch;
-};
-
-
-library.prototype.getAuthors = function () {
-    var filter = {}; //staging object searches for duplicates before passing authors to a new array
-    var Authors=[];//new array of authors in library
-	  for(var i = 0; i < this.myBookArray.length; i++) {
-		    if (!filter[this.myBookArray[i].author]) {//if this does not exist
-			       filter[this.myBookArray[i].author] = true; //then add key/value pair of "author xyz" = true to the n object
-			       Authors.push(this.myBookArray[i].author); //and push, the author into the Authors array
-		    } //do nothing
-	  } return Authors; //show authors
-};
-
-
-
-
-
-  // var d = new Date(99, 5, 24);
-  // //document.getElementById("demo").innerHTML = date
 
 
 
@@ -87,6 +29,9 @@ library.prototype.init = function(){
 };
 
 library.prototype._bindEvents = function(){
+  $("button.show-authors").on("click", $.proxy(this._getAuthors, this));
+  $("button.search-title").on("click", $.proxy(this._getBookByTitle, this));
+  $(".search-author").on("click", $.proxy(this._getBooksByAuthor, this));
   $(".remove-author").on("click", $.proxy(this._removeBookByAuthor, this));
   $(".remove-title").on("click", $.proxy(this._removeBookByTitle, this));
   $("button.randomA").on("click", $.proxy(this._getRandomAuthor, this));
@@ -103,56 +48,56 @@ library.prototype._showBooks = function(){
 };
 
 library.prototype._addBook = function (book) {
-    // var book = new newBook(book[0], book[1], book[2], book[3]);
-    // if (book.length >= 4){
+    if (this.myBookArray.length){
         for(var i = 0; i < this.myBookArray.length; i++){
             if(this.myBookArray[i].title == book.title) {
     				    return false;
 
                 }
-            } console.log(book);
+            }
             this.myBookArray.push(book);
             return true;
-        };
+        } else {this.myBookArray.push(book);                  //if library is empty add the book
+            return true;
+          }
+};
 
 
+library.prototype._addBooks = function(){
+    var books = [];
+    var booksAdded = [];
+    var notAdded = [];
+    $.each($(".forms form"),function(Index, val){
+        var $form = $(this);
+        var title = $form.find(".title").val();
+        var author = $form.find(".author").val();
+        var page = $form.find(".pages").val();
+        var date = $form.find(".date").val();
 
+        books.push(new newBook(title, author, page, date));
+  });
 
-// library.prototype._addBooks = function(){
-//     var books = [];
-//     var booksAdded = [];
-//     var notAdded = [];
-//     $.each($(".forms form"),function(Index, val){
-//         var $form = $(this);
-//         var title = $form.find(".title").val();
-//         var author = $form.find(".author").val();
-//         var page = $form.find(".pages").val();
-//         var date = $form.find(".date").val();
-//
-//         books.push(new newBook(title, author, page, date));
-//   });
-//
-//     for(var j=0; j < books.length; j++){
-//         var bookTitle = books[j].title;
-//         if(this._addBook(books[j])){
-//           booksAdded.push(bookTitle);
-//           console.log(booksAdded);
-//         } else {
-//           notAdded.push(bookTitle);
-//         }
-// 	  }
-//
-//     if(notAdded.length){
-//         var stringTitle = [];
-//         for(var p = 0; p < notAdded.length; p++){
-//           stringTitle.push(notAdded[p]);
-//         }
-//         $("#results").append("<li id='results'>" + stringTitle.join("") + " are already in the library.</li>");//use stringTitle im message to display book titles in message
-//       } else {
-//           $("#results").append("<li id='results'>All books were added to the library.</li>");
-//       } //display message that all books were added
-//       return this._showBooks();
-// };
+    for(var j=0; j < books.length; j++){
+        var bookTitle = books[j].title;
+        if(this._addBook(books[j])){
+          booksAdded.push(bookTitle);
+          console.log(booksAdded);
+        } else {
+          notAdded.push(bookTitle);
+        }
+	  }
+
+    if(notAdded.length){
+        var stringTitle = [];
+        for(var p = 0; p < notAdded.length; p++){
+          stringTitle.push(notAdded[p]);
+        }
+        $("#results").append("<li id='results'>" + stringTitle.join("") + " are already in the library.</li>");//use stringTitle im message to display book titles in message
+      } else {
+          $("#results").append("<li id='results'>All books were added to the library.</li>");
+      } //display message that all books were added
+      return this._showBooks();
+};
 
 
 
@@ -222,18 +167,75 @@ library.prototype._getRandomAuthor = function () {
     } $("#results").replaceWith("Sorry, no books in the library!");
 };
 
+library.prototype._getBookByTitle = function () {
+    var title = $(".stitle").val();
+    var booksThatMatch = new Array();
+    for (var i = 0; i<this.myBookArray.length; i++) {
+        if (this.myBookArray[i].title.toLowerCase().match(title.toLowerCase()) !== null) {
+            booksThatMatch.push(this.myBookArray[i]);
+        }
+    }//2nd leg of if statement has to be outside of loop to get more than one match
+    console.log(booksThatMatch)
+    if(booksThatMatch.length){
+        var stringTitle = [];
+        for(var p = 0; p < booksThatMatch.length; p++){
+          stringTitle.push(booksThatMatch[p].title);
+        } $("#results").replaceWith("<li id='results'>" + stringTitle.join(" , ") + " are in the library.</li>");
+    } else {
+   $("#results").replaceWith("<li id='results'>There are no books that match that title in the library.</li>");
+    }
+};
+
+
+library.prototype._getBooksByAuthor = function () {
+    var author = $(".sauthor").val();
+    var BooksWithAuthorsThatMatch = new Array();
+    for (var i = 0; i<this.myBookArray.length; i++) {
+        if (this.myBookArray[i].author.toLowerCase().match(author.toLowerCase()) !== null) {
+            BooksWithAuthorsThatMatch.push(this.myBookArray[i]);
+            var AuthorThatMatches = this.myBookArray[i].author;
+        }
+    }
+    if(BooksWithAuthorsThatMatch.length){
+        var stringTitles= [];
+        for(var p = 0; p < BooksWithAuthorsThatMatch.length; p++){
+          stringTitles.push(BooksWithAuthorsThatMatch[p].title);
+        }
+         $("#results").replaceWith("<li id='results'>" + stringTitles.join(" , ") + " by " + AuthorThatMatches + " are in the library.</li>");
+    } else {
+   $("#results").replaceWith("<li id='results'>There are no authors with that name in the library.</li>");
+    }
+};
+
+library.prototype._getAuthors = function () {
+    var filter = {}; //staging object searches for duplicates before passing authors to a new array
+    var Authors=[];//new array of authors in library
+	  for(var i = 0; i < this.myBookArray.length; i++) {
+		    if (!filter[this.myBookArray[i].author]) {//if this does not exist
+			       filter[this.myBookArray[i].author] = true; //then add key/value pair of "author xyz" = true to the n object
+			       Authors.push(this.myBookArray[i].author); //and push, the author into the Authors array
+		    } //do nothing
+	  } console.log (Authors)
+    var stringAuthors= [];
+      for(var p = 0; p < Authors.length; p++){
+      stringAuthors.push(Authors[p]);
+    }
+     $("#results").replaceWith("<li id='results'>" + stringAuthors.join(" , ") + ".</li>");
+};
+
+
 
 
 
 var gnewLibrary = new library();
 
-var gbook1 = ["1987","George Orwell", 3, "1944/03/25"];
-var gbook2 = ["Duck Farm","George Orwell", 3, "1902/01/20"];
-var gbook3 = ["The Crucible","Arthur Miller", 3, "1852/04/20"];
-var gbook4 = ["Bling Money","George Orwell", 3, "1999/04/01"];
-var gbook5 = ["Zen and the Art of Motorcycle Maintenance","Robert M. Persig", 3, "1986/11/15"];
-var gbook6 = ["The Crucified Church","Joel L. Rissinger", 500, "2011/02/01"];
-var gbook7 = ["The Social Meaning of Money: Pin Money, Paychecks, Poor Relief, and Other Currencies", "Viviana A. Zelizer", 420,"1997/08/30"];
+var gbook1 = new newBook ("1987","George Orwell", 3, "1944/03/25");
+var gbook2 = new newBook("Duck Farm","George Orwell", 3, "1902/01/20");
+var gbook3 = new newBook("The Crucible","Arthur Miller", 3, "1852/04/20");
+var gbook4 = new newBook("Bling Money","George Orwell", 3, "1999/04/01");
+var gbook5 = new newBook("Zen and the Art of Motorcycle Maintenance","Robert M. Persig", 3, "1986/11/15");
+var gbook6 = new newBook("The Crucified Church","Joel L. Rissinger", 500, "2011/02/01");
+var gbook7 = new newBook("The Social Meaning of Money: Pin Money, Paychecks, Poor Relief, and Other Currencies", "Viviana A. Zelizer", 420,"1997/08/30");
 
 
 library.prototype.addBooksOnPageLoad = function(){
